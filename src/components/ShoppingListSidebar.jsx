95b8ff614af2489b53f1457adbbe223525e0042f
jlinares30 <jorgelinaresrrs306@hotmail.com>
2025-03-06 08:00:16 -0500

    shopping sidebar added and data created

--- src/components/ShoppingListSidebar.jsx
+++ src/components/ShoppingListSidebar.jsx
@@ -3,12 +3,71 @@ import style from '../styles/ShoppingListSidebar.module.css';
 function ShoppingListSidebar() {
   return (
     <div className={style.shoppingListSidebar}>
-      <h2>Shopping List</h2>
-      <ul>
-        <li>Apples</li>
-        <li>Bananas</li>
-        <li>Carrots</li>
+      <h2 className={style.title}>Shopping List</h2>
+      <ul className={style.shoppingList}>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
+        <li className={style.itemList}>
+          <div className={style.nameDotsContainer}>
+            <img src="src/assets/grid-dots.svg" alt="grid-dots" />
+            <div className={style.nameItem}>Apples</div>
+          </div>
+          <div className={style.price}>2</div>
+        </li>
       </ul>
+      <div className={style.buttonContainer}>
+        <button className={style.button}>Clear List</button>
+      </div>
+      <div className={style.budget}>
+        Estimated Budget: $50
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,24 +8,30 @@ function Sidebar({ location }) {
       [style.sidebar]: location !== "home",
     }
   );
+  const navItemClasses = classNames(
+    style.navItemHome,
+    {
+      [style.navItem]: location !== "home",
+    }
+  );
   return (
     <aside className={sidebarClasses}>
       <h2 className={style.title} >Home</h2>
       <nav className={style.nav}>
         <ul className={style.navList}>
-          <li className={style.navItem}>
+          <li className={navItemClasses}>
             <img src="src/assets/receipt.svg" alt="recipe icon" />
             <p>Recipes</p>
           </li>
-          <li className={style.navItem}>
+          <li className={navItemClasses}>
             <img src="src/assets/avocado.svg" alt="ingredient icon" />
             <p>Ingredients</p>
           </li>
-          <li className={style.navItem}>
+          <li className={navItemClasses}>
             <img src="src/assets/list-check.svg" alt="list check icon" />
             <p>Shopping List</p>
           </li>
-          <li className={style.navItem}>
+          <li className={navItemClasses}>
             <img src="src/assets/calendar-week.svg" alt="calendar week icon" />
             <p>Planner</p>
           </li>
